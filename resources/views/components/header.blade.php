c05ec6ca5499762aed0a33a86c95677f5c2d9e9a
Hiddekel06 <paulmbaye393@gmail.com>
2025-12-11 11:03:39 +0000

    Header responsive

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,14 @@
-<header class="w-full p-6 font-poppins">
+<header class="w-full p-3 sm:p-4 md:p-6 font-poppins">
     <div class="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <!-- Logo -->
-        <a href="{{ route('vote.index') }}" class="mb-4 md:mb-0">
-            <img src="{{ asset('images/LogoGova.jpeg') }}" alt="Logo GovAthon" class="h-16">
+        <a href="{{ route('vote.index') }}" class="mb-3 md:mb-0">
+            <img src="{{ asset('images/LogoGova.jpeg') }}" alt="Logo GovAthon" class="h-12 sm:h-14 md:h-16">
         </a>
         
         
 
         <!-- Navigation -->
-        <nav class="flex justify-center items-center space-x-8 text-lg">
+        <nav class="flex justify-center items-center space-x-2 sm:space-x-4 md:space-x-8 text-xs sm:text-sm md:text-lg flex-wrap">
             <a href="{{ route('vote.index') }}" 
                class="font-semibold hover:text-emerald-300 transition-colors duration-300 [text-shadow:_0_1px_10px_rgb(255_255_255_/_30%)] 
                       {{ request()->routeIs('vote.index') || request()->routeIs('vote.secteurs') ? 'text-white' : 'text-gray-300' }}">
